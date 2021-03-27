7c82ad90c899be090bb6b958dddf45537e737504
Harris Munahar <harris.munahar@raharja.info>
2021-03-27 21:46:51 +0700

    '-'

---
+++ PBO6-2022436280-Latihan58-TambahKurang/src/pbo6/pkg2022436280/latihan58/tambahkurang/Bilangan.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6.pkg2022436280.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Funtsu
+ */
+public class Bilangan {
+    
+}
